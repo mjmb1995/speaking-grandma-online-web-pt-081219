3bd83371d474c6c4d88c4fd6d8c226beef8f4950
Mark Bihag <markbihag330@gmail.com>
2019-08-14 04:36:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 #speak_to_grandma
 def speak_to_grandma(string)
   if string != string.uppercase
-    puts "HUH? SPEAK UP, SONNY!"
+    return "HUH? SPEAK UP, SONNY!"
   elsif string == string.uppercase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
 
